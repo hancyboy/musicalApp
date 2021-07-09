6d7736884df2fc96158a4140f627c87a1472212f
umesh <raiumesh470@gmail.com>
2021-07-09 15:12:31 +0545

    test commit

---
+++ lib/SongList.dart
@@ -0,0 +1,23 @@
+/*import 'package:flutter/material.dart';*/
+
+class Song {
+  String title;
+  String description;
+
+  Song({
+    required this.title,
+    required this.description,
+  });
+}
+
+List<Song> songList = [
+  Song(title: 'Perfect', description: 'This is a song by Annie marie'),
+  Song(title: 'Behos', description: 'This is a song by Annie marie'),
+  Song(title: 'Love me', description: 'This is a song by Annie marie'),
+  Song(title: 'Sathi', description: 'This is a song by Annie marie'),
+  Song(title: 'Budi', description: 'This is a song by Annie marie'),
+  Song(title: 'Photography', description: 'This is a song by Annie marie'),
+  Song(title: 'Happier', description: 'This is a song by Annie marie'),
+  Song(title: 'Namo Namo', description: 'This is a song by Annie marie'),
+  Song(title: 'Humdard', description: 'This is a song by Annie marie'),
+];
