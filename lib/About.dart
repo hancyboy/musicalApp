18cabb9bc028600201a22ba6963da695d0dd1e1a
umesh <raiumesh470@gmail.com>
2021-07-15 13:19:23 +0545

    test commit

--- lib/About.dart
+++ lib/About.dart
@@ -9,8 +9,8 @@ class About extends StatelessWidget {
           title: new Text('About Developer!'),
           centerTitle: true,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
+        body: Container(
+          margin: EdgeInsets.all(20),
           child: new Text(
             " This is the Lyrical android application develop by Umesh Rai and Bibek Kaliraj. We are the students of Computer Science of Pulchowk Engineering Campus",
             style: TextStyle(
--- lib/Welcome.dart
+++ lib/Welcome.dart
@@ -4,19 +4,20 @@ class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          backgroundColor: Colors.purple,
-          title: Text(
-            "Welcome Page!",
-          ),
-          centerTitle: true,
+      appBar: new AppBar(
+        backgroundColor: Colors.purple,
+        title: Text(
+          "Welcome Page!",
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: new Text(
-            " This is a android application where you can find tons of lyrics of Hindi, Nepali and English songs!",
-            style: TextStyle(fontSize: 15),
-          ),
-        ));
+        centerTitle: true,
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: new Text(
+          " This is a android application where you can find tons of lyrics of Hindi, Nepali and English songs!",
+          style: TextStyle(fontSize: 15),
+        ),
+      ),
+    );
   }
 }
